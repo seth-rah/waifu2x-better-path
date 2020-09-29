5e512b0cfde6d427ef61ff2d82c4131125ae8de1
Seth <seth.evangelion.rah@gmail.com>
2020-09-29 14:45:19 +0200

    Expanded Readme
    Updated featureset script to check for "extra", "slow" or "all" as a second argument to use more methods.
    
    Added colour to prompts.

--- README.md
+++ README.md
@@ -23,15 +23,20 @@ Once everything in the installation is taken care of, you can easily access waif
 ### What QOL changes are included in this script?
 
 1. The `waifu` command is aware of the current directory, or the full directory path provided.
-2. `waifu` can be called with or without a filename before script execution. 
+2. Will default to enabling the following arguments on all rescales, except for step 5 having no defaults set aside from the ones that came with [waifu2x ncnn vulkan](https://github.com/nihui/waifu2x-ncnn-vulkan/releases). `-n 2 -s 2 -v`
+3. `waifu` can be called with or without a filename before script execution. 
    * `waifu filename` or `waifu drive:\filepath\filename` is supported. cmd and powershell also support click dragging a file into their window to populate the full file path. short path only supported if CMD / Powershell are open in that files directory.
    * `waifu` without any arguments is supported and will request a filename afterwards
-3. Automatically assumes the file name provided ends in png or jpg. If both or neither exist, script will request you to provide the file extension.
+4. Automatically assumes the file name provided ends in `png` or `jpg`. If both or neither exist, script will request you to provide the file extension.
    * If the file extension is provided with or without a fullstop, the script will handle it appropriately and add it if it was missing.
    * If the filename exists but the extension was provided incorrectly, it will ask you to provide the extension again.
-4. Providing any secondary arguments after the filename on script launch will give you the ability to freely select the arguments you wish to use. to enable free arguments, use the command as follows `waifu filename anythingGoesHere`.
-5. You only need to provide an input filename, the output will store in the same directory as the input file, with a `-waifu` suffixed to the filename. 
+5. Providing any secondary arguments aside from `extra`, `slow` or `all` after the filename on script launch will give you the ability to freely select the arguments you wish to use. to enable free arguments, use the command as follows `waifu filename args`.
+6. You only need to provide an input filename, the output will store in the same directory as the input file, with a `-waifu` suffixed to the filename. 
    * If the filename already exists, the script will ask if you wish to overwrite the file that's already there, or to cancel the operation.
    * A future version might request you which filename you'd prefer instead of overwriting, or suffix a value to not conflict with the existing file.
+7. Upscale using cunet, photo and anime methods in one command by appending the expected argument.
+   * Adding `extra` to the 2nd waifu argument will create a `-cunetwaifu`, `-animewaifu` and `-photowaifu` file.
+   * Adding `slow` to the 2nd waifu argument will create a `-ttacunetwaifu`, `-ttaanimewaifu` and `-ttaphotowaifu` file. This gets handled the same as `extra`, except the `-x` flag is enabled.
+   * Adding `all` to the 2nd waifu argument will create a `-cunetwaifu`, `-animewaifu`, `-photowaifu`, `-ttacunetwaifu`, `-ttaanimewaifu` and `-ttaphotowaifu` file. It handles running `slow` and `extra` logic.
 
-In short, you just need to `shift - rightclick` on an empty space in a folder containing the file you wish to rescale, select `Open PowerShell window here`. Type `waifu filename` and you're set. If you don't know the filename, `click drag` the image into the `PowerShell` window instead of typing the filename, or with CMD if that's your cup of tea.
+In short, you just need to `shift - rightclick` on an empty space in a folder containing the file you wish to rescale, select `Open PowerShell window here`. Type `waifu filename argument` and you're set. If you don't know the filename, `click drag` the image into the `PowerShell` window instead of typing the filename, or with CMD if that's your cup of tea.
--- waifu.bat
+++ waifu.bat
@@ -1,9 +1,14 @@
 @ECHO OFF
-SETLOCAL
+SETLOCAL 
+:SETCOLOURS
+FOR /F "tokens=1,2 delims=#" %%a in ('"PROMPT #$H#$E# & ECHO ON & FOR %%b IN (1) DO REM"') DO ( SET COL=%%b )
+
+:CHECKINPUT
 IF "%~1"=="" (GOTO FILE) ELSE (GOTO DECLARE)
 
 :DECLARE
 SET file=%1
+SET args=%2
 GOTO GETVARIABLES
 
 :FILE
@@ -17,6 +22,9 @@ SET filepath=%%~pi
 SET filename=%%~ni
 SET fileextension=%%~xi
 )
+GOTO CHECKEXTENSION
+
+:CHECKEXTENSION
 IF "%fileextension%"=="" (GOTO TESTEXTENSION) ELSE (GOTO CHECKEXIST)
 
 :TESTEXTENSION
@@ -35,55 +43,165 @@ SET fileextension=.%fileextension%
 GOTO CHECKEXIST
 
 :CHECKEXIST
-IF NOT EXIST "%filedrive%%filepath%%filename%.*" (GOTO NOTFOUND)
-IF NOT EXIST "%filedrive%%filepath%%filename%%fileextension%" (GOTO EXTNOTFOUND)
-IF EXIST "%filedrive%%filepath%%filename%-waifu%fileextension%" (GOTO EXISTS) ELSE (GOTO WAIFUCHECK)
+IF /I NOT EXIST "%filedrive%%filepath%%filename%.*" (GOTO NOTFOUND)
+IF /I NOT EXIST "%filedrive%%filepath%%filename%%fileextension%" (GOTO EXTNOTFOUND)
+IF /I "%args%"=="" IF EXIST "%filedrive%%filepath%%filename%-cunetwaifu%fileextension%" (ECHO. & ECHO %COL%[91m%filename%-cunetwaifu%fileextension% already exists.%COL%[0m & GOTO EXISTS)
+IF /I "%args%"=="extra" IF EXIST "%filedrive%%filepath%%filename%-cunetwaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-cunetwaifu%fileextension% / %filename%-animewaifu%fileextension% / %filename%-photowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS)
+IF /I "%args%"=="extra" IF EXIST "%filedrive%%filepath%%filename%-animewaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-cunetwaifu%fileextension% / %filename%-animewaifu%fileextension% / %filename%-photowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS)
+IF /I "%args%"=="extra" IF EXIST "%filedrive%%filepath%%filename%-photowaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-cunetwaifu%fileextension% / %filename%-animewaifu%fileextension% / %filename%-photowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS) 
+IF /I "%args%"=="slow" IF EXIST "%filedrive%%filepath%%filename%-ttacunetwaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-ttacunetwaifu%fileextension% / %filename%-ttanimewaifu%fileextension% / %filename%-ttaphotowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS)
+IF /I "%args%"=="slow" IF EXIST "%filedrive%%filepath%%filename%-ttaanimewaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-ttacunetwaifu%fileextension% / %filename%-ttaanimewaifu%fileextension% / %filename%-ttaphotowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS)
+IF /I "%args%"=="slow" IF EXIST "%filedrive%%filepath%%filename%-ttaphotowaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-ttacunetwaifu%fileextension% / %filename%-ttaanimewaifu%fileextension% / %filename%-ttaphotowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS) 
+IF /I "%args%"=="all" IF EXIST "%filedrive%%filepath%%filename%-cunetwaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-cunetwaifu%fileextension% / %filename%-animewaifu%fileextension% / %filename%-photowaifu%fileextension% / %filename%-ttacunetwaifu%fileextension% / %filename%-ttaanimewaifu%fileextension% / %filename%-ttaphotowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS)
+IF /I "%args%"=="all" IF EXIST "%filedrive%%filepath%%filename%-animewaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-cunetwaifu%fileextension% / %filename%-animewaifu%fileextension% / %filename%-photowaifu%fileextension% / %filename%-ttacunetwaifu%fileextension% / %filename%-ttaanimewaifu%fileextension% / %filename%-ttaphotowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS)
+IF /I "%args%"=="all" IF EXIST "%filedrive%%filepath%%filename%-photowaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-cunetwaifu%fileextension% / %filename%-animewaifu%fileextension% / %filename%-photowaifu%fileextension% / %filename%-ttacunetwaifu%fileextension% / %filename%-ttaanimewaifu%fileextension% / %filename%-ttaphotowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS) 
+IF /I "%args%"=="all" IF EXIST "%filedrive%%filepath%%filename%-ttacunetwaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-cunetwaifu%fileextension% / %filename%-animewaifu%fileextension% / %filename%-photowaifu%fileextension% / %filename%-ttacunetwaifu%fileextension% / %filename%-ttanimewaifu%fileextension% / %filename%-ttaphotowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS)
+IF /I "%args%"=="all" IF EXIST "%filedrive%%filepath%%filename%-ttaanimewaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-cunetwaifu%fileextension% / %filename%-animewaifu%fileextension% / %filename%-photowaifu%fileextension% / %filename%-ttacunetwaifu%fileextension% / %filename%-ttaanimewaifu%fileextension% / %filename%-ttaphotowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS)
+IF /I "%args%"=="all" IF EXIST "%filedrive%%filepath%%filename%-ttaphotowaifu%fileextension%" (ECHO. & ECHO %COL%[91mAny of the following  "%filename%-cunetwaifu%fileextension% / %filename%-animewaifu%fileextension% / %filename%-photowaifu%fileextension% / %filename%-ttacunetwaifu%fileextension% / %filename%-ttaanimewaifu%fileextension% / %filename%-ttaphotowaifu%fileextension%" already exist.%COL%[0m & GOTO EXISTS) 
+ECHO made to end
+IF /I "%args%" NEQ "extra" (IF /I "%args%" NEQ "slow" (IF /I "%args%" NEQ "all" (IF /I "%args%" NEQ "" (IF EXIST "%filedrive%%filepath%%filename%-argwaifu%fileextension%" (ECHO. & ECHO %COL%[91m%filename%-argwaifu%fileextension% already exists.%COL%[0m & GOTO EXISTS)))))
+GOTO WAIFUCHECK
 
 :EXISTS
-ECHO %filedrive%%filepath%%filename%-waifu%fileextension% already exists.
-SET /P exists=Do you wish to overwrite the file (Y/N)?
+ECHO.
+IF /I "%args%" EQU "extra" SET /P exists=Do you wish to overwrite all files (Y/N)? 
+IF /I "%args%" EQU "slow" SET /P exists=Do you wish to overwrite all files (Y/N)? 
+IF /I "%args%" EQU "all" SET /P exists=Do you wish to overwrite all files (Y/N)? 
+IF /I "%args%" NEQ "extra" IF /I "%args%" NEQ "slow" IF /I "%args%" NEQ "all" SET /P exists=Do you wish to overwrite the file (Y/N)? 
 IF /I "%exists%" NEQ "Y" (GOTO STOPPING) ELSE (GOTO WAIFUCHECK)
 
 :WAIFUCHECK
-IF "%~2"=="" (GOTO WAIFU)
+IF "%args%"=="" (GOTO WAIFU)
+IF "%args%"=="extra" (GOTO WAIFUEXT)
+IF "%args%"=="slow" (GOTO WAIFUSLOW)
+IF "%args%"=="all" (GOTO WAIFUALL)
 ECHO.  
-ECHO.
-ECHO "Free arguments enabled"
-ECHO "File output path is set in stone"
-ECHO "  -v                   verbose output"
-ECHO "  -n noise-level       denoise level (-1/0/1/2/3, default=0)"
-ECHO "  -s scale             upscale ratio (1/2, default=2)"
-ECHO "  -t tile-size         tile size (>=32/0=auto, default=0) can be 0,0,0 for multi-gpu"
-ECHO "  -m model-path        waifu2x model path (default=models-cunet)"
-ECHO "  -g gpu-id            gpu device to use (default=0) can be 0,1,2 for multi-gpu"
-ECHO "  -j load:proc:save    thread count for load/proc/save (default=1:2:2) can be 1:2,2,2:2 for multi-gpu"
-ECHO "  -x                   enable tta mode"
-ECHO "  -f format            output image format (jpg/png/webp, default=ext/png)"
+ECHO %COL%[95m"Free arguments enabled"%COL%[0m
+ECHO %COL%[95m"File output path is set in stone"%COL%[0m
+ECHO %COL%[95m"  -v                   verbose output"%COL%[0m
+ECHO %COL%[95m"  -n noise-level       denoise level (-1/0/1/2/3, default=0)"%COL%[0m
+ECHO %COL%[95m"  -s scale             upscale ratio (1/2, default=2)"%COL%[0m
+ECHO %COL%[95m"  -t tile-size         tile size (>=32/0=auto, default=0) can be 0,0,0 for multi-gpu"%COL%[0m
+ECHO %COL%[95m"  -m model-path        waifu2x model path (default=models-cunet)"%COL%[0m
+ECHO %COL%[95m"  -g gpu-id            gpu device to use (default=0) can be 0,1,2 for multi-gpu"%COL%[0m
+ECHO %COL%[95m"  -j load:proc:save    thread count for load/proc/save (default=1:2:2) can be 1:2,2,2:2 for multi-gpu"%COL%[0m
+ECHO %COL%[95m"  -x                   enable tta mode"%COL%[0m
+ECHO %COL%[95m"  -f format            output image format (jpg/png/webp, default=ext/png)"%COL%[0m
 ECHO.  
-ECHO. 
 SET /P args="Provide extra arguments for rescaling. Leave empty for waifu2x-ncnn-vulkan default: "
 GOTO WAIFUARGS
 
 :WAIFUARGS
+ECHO. 
 @ECHO ON
-waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-waifu%fileextension% %args%
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-argwaifu%fileextension% %args%
 @ECHO OFF
+ECHO. 
+ECHO %COL%[93mRescaling complete%COL%[0m
+ENDLOCAL
 cmd /k
 
 :WAIFU
+ECHO. 
 @ECHO ON
-waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-waifu%fileextension% -n 2 -s 2 -v
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-cunetwaifu%fileextension% -n 2 -s 2 -v
 @ECHO OFF
+ECHO. 
+ECHO %COL%[93mRescaling complete%COL%[0m
+ECHO. 
+ENDLOCAL
+cmd /k
+
+:WAIFUEXT
+ECHO. 
+ECHO %COL%[104;30m Three commands are about to run in sequence. Please be patient. %COL%[0m
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-cunetwaifu%fileextension% -n 2 -s 2 -v
+@ECHO OFF
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-animewaifu%fileextension% -n 2 -s 2 -v -m models-upconv_7_anime_style_art_rgb
+@ECHO OFF
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-photowaifu%fileextension% -n 2 -s 2 -v -m models-upconv_7_photo
+@ECHO OFF
+ECHO. 
+ECHO %COL%[93mRescaling complete%COL%[0m
+ECHO. 
+ENDLOCAL
+cmd /k
+
+:WAIFUSLOW
+ECHO. 
+ECHO %COL%[104;30m Three very slow commands are about to run in sequence. Please be patient. %COL%[0m
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-ttacunetwaifu%fileextension% -n 2 -s 2 -v -x
+@ECHO OFF
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-ttaanimewaifu%fileextension% -n 2 -s 2 -v -x -m models-upconv_7_anime_style_art_rgb
+@ECHO OFF
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-ttaphotowaifu%fileextension% -n 2 -s 2 -v -x -m models-upconv_7_photo
+@ECHO OFF
+ECHO. 
+ECHO %COL%[93mRescaling complete%COL%[0m
+ECHO. 
+ENDLOCAL
+cmd /k
+
+:WAIFUALL
+ECHO. 
+ECHO %COL%[104;30m Six commands are about to run in sequence. This will take a while, please be patient. %COL%[0m
+ECHO.
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-cunetwaifu%fileextension% -n 2 -s 2 -v
+@ECHO OFF
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-animewaifu%fileextension% -n 2 -s 2 -v -m models-upconv_7_anime_style_art_rgb
+@ECHO OFF
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-photowaifu%fileextension% -n 2 -s 2 -v -m models-upconv_7_photo
+@ECHO OFF
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-ttacunetwaifu%fileextension% -n 2 -s 2 -v -x
+@ECHO OFF
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-ttaanimewaifu%fileextension% -n 2 -s 2 -v -x -m models-upconv_7_anime_style_art_rgb
+@ECHO OFF
+ECHO. 
+@ECHO ON
+waifu2x-ncnn-vulkan -i %filedrive%%filepath%%filename%%fileextension% -o %filedrive%%filepath%%filename%-ttaphotowaifu%fileextension% -n 2 -s 2 -v -x -m models-upconv_7_photo
+@ECHO OFF
+ECHO. 
+ECHO %COL%[93mRescaling complete%COL%[0m
+ECHO. 
+ENDLOCAL
 cmd /k
 
 :EXTNOTFOUND
-ECHO %filedrive%%filepath%%filename% exists, but %fileextension% is not correct, please provide the correct file extension.
+ECHO. 
+ECHO %COL%[91m%filedrive%%filepath%%filename% exists, but %fileextension% is not correct, please provide the correct file extension.%COL%[0m
+ECHO. 
 GOTO SETEXTENSION
 
 :NOTFOUND
-ECHO Input file not found, please try again.
+ECHO. 
+ECHO %COL%[91mInput file not found, please try again.%COL%[0m
+ECHO. 
 GOTO FILE
 
 :STOPPING
-ECHO closing
+ECHO.
+ECHO %COL%[91mclosing%COL%[0m
+ECHO. 
+ENDLOCAL
 cmd /k
